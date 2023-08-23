7c60b53b59d727a17b0cd1e7bd5992b92d77acbd
Nneoma01 <toughspirit>
2023-08-23 17:44:21 +0100

    theme color change

--- calculator.js
+++ calculator.js
@@ -1,23 +1,11 @@
-// const button = document.querySelectorAll("button")
-// const calAnswer = document.querySelector(".cal-answer")
-
-
-// const displayValue = (value) => {
-//     if (value === "AC") {
-//       calAnswer.style.display = "AC";
-//     } else {
-//       calAnswer.style.display = value;
-//     }
-//   }
-  
-//   button.forEach(button => {
-//     button.addEventListener("click", e => {
-//       const value = button.getAttribute("value")
-//       displayValue(value)
-//     })
-//   })
-
-const outputScreen = document.getElementById("output-screen");
+const outputScreen = document.getElementById("outputScreen");
+const bright = document.getElementById("bright");
+const dark = document.getElementById("dark");
+const container = document.querySelector("container")
+const icon = document.querySelectorAll("icon")
+const calSymbols = document.querySelector("cal-symbols")
+const fontColor = document.querySelectorAll("font-color")
+const button = document.querySelectorAll("button")
 
 function display(num) {
     outputScreen.value += num;
@@ -32,6 +20,27 @@ function Clear() {
 }
 
 function Delete() {
-    outputScreen.value = outputScreen.value.slice()
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
+
+function darkMode() {
+    alert("color changes")
+    // container.style.backgroundColor = "black";
+
+}
+
+function brightMode() {
+    alert("color changes")
+    //     container.style.backgroundColor  = "white"
+    //     icon.style.backgroundColor = "whitesmoke"
+    //     icon.style.color = "black"
+    //     calSymbols.style.background = "whitesmoke"
+    //     button.style.background = "white"
+    //     fontColor.style.color = "black" 
+    //     outputScreen.style.color = "black"
+    //     dark.style.opacity = "0.6"
+
+    // container.classList.add("bright-container")
+    // icon.classList.add("")
 }
 
